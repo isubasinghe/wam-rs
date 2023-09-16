47470712d0c943cf80c3962b8947224ce380eb12
isubasinghe <isubasinghe@student.unimelb.edu.au>
2023-09-16 19:16:50 +1000

    implemented put_structure,set_variable,set_value

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+#![allow(dead_code)]
 
+use std::{sync::Arc, collections::{HashMap, HashSet}, cell::RefCell};
 
 pub enum  Variable {
     Bounded(usize), 
@@ -6,24 +8,110 @@ pub enum  Variable {
 }
 
 
-pub enum DataCell {
+pub enum DataCellX {
     // <REF, k>
-    Var(String, Variable),
+    Var(Variable),
     // <STR, k>
     StructIdx(usize),
     // Name, Arity
     StructDef(String, usize),
 }
 
+pub type DataCell = Arc<DataCellX>;
 
-pub struct Heap {
-    cells: Vec<DataCell>
+
+pub struct RegisterX {
+    num: usize, 
+    data: Option<DataCell>
 }
 
-impl Heap {
-    fn index(&self, idx: usize) -> &DataCell {
-        &self.cells[idx] // maybe panic
+impl RegisterX {
+    fn load(&self) -> Option<DataCell> {
+        self.data.clone()
     }
+
+    fn store(&mut self, val: DataCell) {
+        self.data = Some(val);
+    }
+}
+
+pub type Register = Arc< RefCell<RegisterX>>;
+
+
+pub struct FlatQuery {
+   name: String,
+   args: Vec<Register>
+}
+
+impl FlatQuery {
+    fn size(&self) -> usize {
+        1
+    }
+}
+
+
+
+pub struct Machine {
+    cells: Vec<DataCell>,
+    variables: HashMap<Arc<String>, Register>,
+    registers: HashSet<Register>, 
+    register_num: usize, 
+    h: usize
+}
+
+impl Machine {
+    fn fresh_reg(&mut self) -> Register {
+        let r: Arc<RefCell<RegisterX>> = RefCell::new(RegisterX{num: self.register_num, data: None.into()}).into();
+        self.register_num += 1;
+        r
+    }
+
+
+
+    fn put_structure(&mut self, q: FlatQuery, reg: Register) {
+        // put structure
+        // (STR, H + 1)
+        let struct_idx = DataCellX::StructIdx(self.h + 1).into();
+        // HEAP[H] = f/n
+        self.cells[self.h] = struct_idx;
+        
+        //Xi <- HEAP[h]
+        let reg_val = self.cells[self.h].clone();
+        reg.borrow_mut().store(reg_val);
+
+        // H <- H + 2
+        self.h = self.h + 2;
+    }
+
+    fn set_variable(&mut self, reg: Register) {
+        // HEAP[H] <- (REF, H)
+        let var = DataCellX::Var(Variable::Unbounded()).into();
+        self.cells[self.h] = var;
+
+        // Xi <- HEAP[h];
+        let var = self.cells[self.h].clone();
+        reg.borrow_mut().store(var);
+        self.h += 1;
+    }
+
+    fn set_value(&mut self, reg: Register) {
+        // HEAP[H] <- Xi; // correctness condition lies on caller
+        let data = reg.borrow().load().unwrap();
+        self.cells[self.h] = data;
+        // H <- H + 1;
+        self.h += 1;
+    }
+
+    fn get_structure() {}
+
+    fn unify_variable() {}
+
+    fn unify_valeu() {}
+
+
+
+
+    fn compile_query() {}
 }
 
 fn main() {
